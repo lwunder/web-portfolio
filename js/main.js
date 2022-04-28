ab0687d0591518cc4b3ec1cfa3f923382f7bf80f
Alice Hong <alicehon@andrew.cmu.edu>
2022-04-28 02:21:27 -0400

    Added popover to Email button in footer.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ function copyEmailToClipboard() {
   var email = "alicehon@andrew.cmu.edu";
   navigator.clipboard.writeText(email);
 }
+
+
+/* copied from https://www.w3schools.com/bootstrap5/bootstrap_popover.php */
+/* popover animation for Email button */
+function loadFunctions() {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+}
